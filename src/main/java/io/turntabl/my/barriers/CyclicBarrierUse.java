3b3061ced85f8e3f4cdb12df74a77655ff57e731
patricia-agyekum <patriciakasapa07@gmail.com>
2019-11-28 18:25:08 +0000

    Main method editted badly

--- src/main/java/io/turntabl/my/barriers/CyclicBarrierUse.java
+++ src/main/java/io/turntabl/my/barriers/CyclicBarrierUse.java
@@ -7,7 +7,12 @@ public class CyclicBarrierUse {
     public static void main (String[] args){
         int horses = 3;
         CyclicBarrier cb = new CyclicBarrier(horses, () ->
-                System.out.println("All done now so you can go home...hahaha"));
-        IntStream.range(0, horses).forEach(h -> new GunToStart(h, cb).start());
+                System.out.println("All done now so you can go home...hahaha")
+                );
+        IntStream.range(0, horses).forEach(h -> {
+            new GunToStart(h, cb).start();
+        });
+
+        System.out.println("All set... Ready to run!");
     }
 }
---
+++ src/main/scala/WeatherService.scala
@@ -0,0 +1,6 @@
+class WeatherService
+
+object WeatherService{
+  val rainfall = Map ("Oda" -> 100.0, "Akrofufu" -> 223.2, "Accra" -> 123.6, "Cape Coast" -> 454.3)
+  //def getrainfall (ci)
+}
