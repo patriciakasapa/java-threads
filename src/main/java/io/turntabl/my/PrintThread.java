4cf13d65b008578d24a9530f962d1d821adf8d64
patricia-agyekum <patriciakasapa07@gmail.com>
2019-11-28 13:40:10 +0000

    Question 3 complete

--- src/main/java/io/turntabl/my/PrintThread.java
+++ src/main/java/io/turntabl/my/PrintThread.java
@@ -1,11 +1,20 @@
 package io.turntabl.my;
 
+import java.io.File;
+
 public class PrintThread implements Runnable {
     public static void main(String[] args) throws InterruptedException {
         Thread lister = new Thread(() -> {
-
+            String[] allfiles;
+            File directory = new File("/home/patricia-agyekum/scala-getting-started/");
+            allfiles = directory.list();
+            for (String filename: allfiles) {
+                System.out.println(filename);
+            }
         }
         );
+        lister.start();
+        lister.join();
     }
     @Override
     public void run() {
